7d9df4e9d81858059762ffaf16fcee50562e4eb4
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2018-07-24 20:09:09 +0900

    Fix host-build.sh

--- host-build.sh
+++ host-build.sh
@@ -4,6 +4,6 @@
 VM="madev9-amd64 madev9-i386 madev8-amd64 madev8-i386"
 for v in $VM; do
   cd $HOME/vagrant/$v
-  vagrant ssh -c "sh development/ma-triqs/${PACKAGE}/setup.sh"
-  vagrant ssh -c "sh development/ma-triqs/${PACKAGE}/build.sh"
+  vagrant ssh -c "sh development/ma-${PACKAGE}/setup.sh"
+  vagrant ssh -c "sh development/ma-${PACKAGE}/build.sh"
 done
